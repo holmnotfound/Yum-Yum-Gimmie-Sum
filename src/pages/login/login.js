c80d0cc984456db22c0630d0b8220009c1d0b385
Rasmus Frolov <rasmusfrolov@live.se>
2025-03-11 11:46:39 +0100

    la till funktionalitet för activeUser

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,14 +1,16 @@
+
 import { storeUsers } from "../../../src/utils/usersStorage.js";
 
 document.addEventListener("DOMContentLoaded", function() {
     addEventListeners(); 
 });
 
-
+const users = storeUsers.getUsersInfo();
+console.log(users);
 
 
 // funktion som hämtar användarens, anropar funktion som hämtar testdata, loopar och jämför mail och lösenord
-function validateForm(){
+/*function validateForm(){
     let email = document.getElementById("email").value;  
     let password = document.getElementById("password").value;  
 
@@ -18,18 +20,19 @@ function validateForm(){
         userList.forEach(user => {
             if (email === user.email && password === user.password) {
                 userFound = true;
-
-                alert("Välkommen till appen");
+                
+                alert('Välkommen');
                 console.log( user.username, 'är inloggad');
                 
-                //window.location.href = "/pages/menu/menu.html";
+                window.location.href = "../../../src/pages/menu/menu.html";
             }
         });
 
         if (!userFound) {
             alert("Fel e-post/lösenord");
         }
-}
+}*/
+
 
 function addEventListeners() {
     const submitBtn = document.querySelector(".login-btn"); 
@@ -40,3 +43,35 @@ function addEventListeners() {
         window.location.href="./pages/register.html"
     });  
 }
+
+getActiveUser();
+
+
+function validateForm() {
+    let email = document.getElementById("email").value;  
+    let password = document.getElementById("password").value;  
+
+    let userList = storeUsers.getUsersInfo();
+    
+    let user = userList.find(user => user.email === email && user.password === password);
+
+    if (user) {
+        alert(`${user.username} är inloggad`);
+        setActiveUser(user);
+        getActiveUser();
+        
+    } else {
+        alert("Fel e-post/lösenord");
+    }
+}
+
+
+    function setActiveUser(user){
+        localStorage.setItem("activeUser", JSON.stringify(user));
+    }
+
+    export function getActiveUser(){
+        let activeUser = localStorage.getItem("activeUser");
+        console.log(activeUser);
+        return activeUser;
+    }
