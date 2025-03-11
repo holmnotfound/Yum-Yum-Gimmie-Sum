a5a45c18bb0922e4ba8c26c98d6021e2aac864c2
Amanda Nielsen <amandasn202@gmail.com>
2025-03-11 09:24:00 +0100

    code for orderconfirmation

---
+++ src/pages/orderconfirmation/orderConf.js
@@ -0,0 +1,23 @@
+import { clickHam, closeSidebar, kundkorg } from "../../components/navbar/navbar.js";
+
+// Funktionerna nedan laddar ett ordernummer varje gång sidan laddas om, sparar ordernumret och ser till att det är unikt
+const usedOrderNumbers = new Set();
+
+function generateOrderNumber() {
+    let orderNumber;
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    do {
+        orderNumber = Array.from({ length: 11 }, () => chars[Math.floor(Math.random() * chars.length)]).join('');
+    } while (usedOrderNumbers.has(orderNumber)); // Se till att det är unikt
+
+    usedOrderNumbers.add(orderNumber); //Sparar ordernumret
+    return orderNumber;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const orderNumberElement = document.getElementById("ordernumber");
+    if (orderNumberElement) {
+        orderNumberElement.textContent = "#" + generateOrderNumber();
+    }
+});
