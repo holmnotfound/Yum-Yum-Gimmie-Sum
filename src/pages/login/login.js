b0a4913f5adad0304619acf23c850ece74836c04
Rasmus Frolov <rasmusfrolov@live.se>
2025-03-11 13:59:26 +0100

    lyckad inlogg och sidbyte till menu

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,42 +1,29 @@
-
 import { storeUsers } from "../../../src/utils/usersStorage.js";
+
+//testar och loggar activeUser
+getActiveUser();
+
+//ser till att validateForm byter sida till menu korrekt
+if(window.location.pathname.includes('/src/pages/menu/menu.html')) {
+    buildMenu(menuNew)
+    console.log(menuNew);  
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     addEventListeners(); 
 });
 
+
+//loggar vilka users som finns
 const users = storeUsers.getUsersInfo();
 console.log(users);
-
-
-// funktion som hämtar användarens, anropar funktion som hämtar testdata, loopar och jämför mail och lösenord
-/*function validateForm(){
-    let email = document.getElementById("email").value;  
-    let password = document.getElementById("password").value;  
-
-    let userList = storeUsers.getUsersInfo();
-
-    let userFound = false;
-        userList.forEach(user => {
-            if (email === user.email && password === user.password) {
-                userFound = true;
-                
-                alert('Välkommen');
-                console.log( user.username, 'är inloggad');
-                
-                window.location.href = "../../../src/pages/menu/menu.html";
-            }
-        });
-
-        if (!userFound) {
-            alert("Fel e-post/lösenord");
-        }
-}*/
 
 
 function addEventListeners() {
     const submitBtn = document.querySelector(".login-btn"); 
     submitBtn.addEventListener('click', validateForm);
+
+
     const registerBtn = document.querySelector(".register-btn");
     registerBtn.addEventListener('click', Event => {
         alert('Byter sida till registreringssidan')
@@ -44,10 +31,12 @@ function addEventListeners() {
     });  
 }
 
-getActiveUser();
-
-
+// validerar form genom find, söker efter rätt user och kollar mail & lösen.
+// vid lyckad inloggning - sätter user som activeUser och byter sida till menu
+// om inloggningen misslyckas - alert
 function validateForm() {
+
+
     let email = document.getElementById("email").value;  
     let password = document.getElementById("password").value;  
 
@@ -60,18 +49,19 @@ function validateForm() {
         setActiveUser(user);
         getActiveUser();
         
+        window.location.href = "../../../src/pages/menu/menu.html";
+        
     } else {
         alert("Fel e-post/lösenord");
     }
 }
 
-
-    function setActiveUser(user){
+function setActiveUser(user){
         localStorage.setItem("activeUser", JSON.stringify(user));
-    }
+}
 
-    export function getActiveUser(){
+export function getActiveUser(){
         let activeUser = localStorage.getItem("activeUser");
         console.log(activeUser);
         return activeUser;
-    }
+}
