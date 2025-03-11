3a4fcd451c18b3d3d8f89719aac49792428df1e0
Rasmus Frolov <rasmusfrolov@live.se>
2025-03-11 10:55:05 +0100

    fixed login w/ message

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,34 +1,26 @@
-import { storeUsers } from "../../utils/usersStorage.js";
-import { Customer } from "../../utils/createUsers.js";
-import { randomTimer } from "../../utils/randomNumber.js";
-
-
+import { storeUsers } from "../../../src/utils/usersStorage.js";
 
 document.addEventListener("DOMContentLoaded", function() {
     addEventListeners(); 
 });
 
 
-/*const newUser = new Customer('Rasmus', '123', 'Customer', 'rasmus@123.gmail.com', 'google')
-storeUsers.addUserToStorage(newUser)
-console.log(storeUsers.getUsersInfo());*/
-
 
 
 // funktion som hämtar användarens, anropar funktion som hämtar testdata, loopar och jämför mail och lösenord
-/*function validateForm(){
+function validateForm(){
     let email = document.getElementById("email").value;  
     let password = document.getElementById("password").value;  
 
     let userList = storeUsers.getUsersInfo();
-    
 
     let userFound = false;
         userList.forEach(user => {
             if (email === user.email && password === user.password) {
                 userFound = true;
+
                 alert("Välkommen till appen");
-                console.log('Rasmus är inloggad');
+                console.log( user.username, 'är inloggad');
                 
                 //window.location.href = "/pages/menu/menu.html";
             }
@@ -37,25 +29,6 @@ console.log(storeUsers.getUsersInfo());*/
         if (!userFound) {
             alert("Fel e-post/lösenord");
         }
-}*/
-
-let allUsers = storeUsers.getUsersInfo();
-console.log(allUsers);
-
-function validateForm() {
-    let email = document.getElementById("email").value;  
-    let password = document.getElementById("password").value;  
-
-    let userList = storeUsers.getUsersInfo();
-    
-    let user = userList.find(user => user.email === email && user.password === password);
-
-    if (user) {
-        alert(`${user.username} är inloggad`);
-        
-    } else {
-        alert("Fel e-post/lösenord");
-    }
 }
 
 function addEventListeners() {
@@ -67,20 +40,3 @@ function addEventListeners() {
         window.location.href="./pages/register.html"
     });  
 }
-
-/*async function fetchTestdata(){
-    try{
-        const response = await fetch('https://santosnr6.github.io/Data/yumyumusers.json'); 
-        if(!response.ok) {
-            throw new Error(`HTTP ERROR! Status: ${response.status}`);
-        }
-
-        const data = await response.json();  
-        console.log(data); 
-        return data.users; 
-
-        } catch (error){
-        alert("Error fetching users:", error);
-        return [];
-    }
-}*/
